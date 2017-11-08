ee071327e9104a29f724accf2b28c4448bbbedb9
Kate <ekaterinatokarenko777@gmail.com>
2017-11-08 09:58:02 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,29 +1,30 @@
-
-
-const a = Number(prompt('Input \'a\''));
-const b = Number(prompt('Input \'b\''));
-const c = Number(prompt('Input \'c\''));
-
-function solveQuadr(a, b, c) {
-    let x1, x2;
+const a = Number(prompt('Input \'a\', \'a\' should not be zero'));
+//if (a !== 0) {
+    const b = Number(prompt('Input \'b\''));
+    const c = Number(prompt('Input \'c\''));
+//} else {
+//    return 'Incorrect \'a\' number';
+//}
+function solveQuadr(a, b, c) { 
+    let x, x1, x2;
     const d = disc(a, b, c);
     if (d > 0) {
         const sqrD = Math.sqrt(d);
-        x1 = (-b + sqrD) / 2*a;
-        x2 = (-b - sqrD) / 2*a;
-        return 'x1 = ' + x1 + ', x2 = ' + x2;
+        return 'x1 = ' + (-b + sqrD) / (2*a) + ', x2 = ' + (-b - sqrD) / (2*a);
     } else if (d < 0){
-        return 'Incorrect number';
+        return 'No solution :(';
+    } else {
+        return 'x = ' + -b / 2*a;
     }
 }
-/*
-function disc(a, b, c) {
-    return ?????
+function disc(a, b, c) { 
+    return b*b - 4*a*c;
 }
-
+function prn(let) {
+    document.write(let);
+}
 const result = solveQuadr(a, b, c);
-prn(result);
-*/
+prn('Result: ' + result);
 
 
 

378e4cb4f0940d76083dac5520b80ac220f0412d
Kate <ekaterinatokarenko777@gmail.com>
2017-11-08 14:09:36 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 const a = Number(prompt('Input \'a\', \'a\' should not be zero'));
-//if (a !== 0) {
     const b = Number(prompt('Input \'b\''));
     const c = Number(prompt('Input \'c\''));
-//} else {
-//    return 'Incorrect \'a\' number';
-//}
 function solveQuadr(a, b, c) { 
     let x, x1, x2;
     const d = disc(a, b, c);
@@ -14,7 +10,7 @@ function solveQuadr(a, b, c) {
     } else if (d < 0){
         return 'No solution :(';
     } else {
-        return 'x = ' + -b / 2*a;
+        return 'x = ' + -b / (2*a);
     }
 }
 function disc(a, b, c) { 
